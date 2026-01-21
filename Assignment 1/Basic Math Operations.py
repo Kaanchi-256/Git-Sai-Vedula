b7e33ae954fe4b36f64cd171917e71160233c8f9
Kaanchi-256 <kumar.vedula@gmail.com>
2026-01-21 22:10:40 +0530

    1st git push

---
+++ Assignment 1/Basic Math Operations.py
@@ -0,0 +1,14 @@
+## Perform Basic math options on given two number"
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+print("Addition:", addition)
+print("subtractioin:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", round(division, 2))
